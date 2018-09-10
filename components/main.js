5a20161e1aee86a0b89aa72c3b51f193b8315e83
Richard Zhang <richard.zhang@activenetwork.com>
2018-09-10 15:25:02 +0800

    Initial Commit

---
+++ components/main.js
@@ -0,0 +1,26 @@
+// components/main.js
+Component({
+  options: {
+    multipleSlots: true
+  },
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
